6d11ca95f559b92e3ff1230329c9afada5037d1f
Jason Stone <shamansanchez@gmail.com>
2019-10-28 23:46:59 -0400

    write yaml files instead of printing to stdout

--- sbubby.py
+++ sbubby.py
@@ -4,9 +4,11 @@ import math
 import sys
 import yaml
 
-'''
+"""
 Function used to calculate distance between two stops
-'''
+"""
+
+
 def weight(u, v, d):
     ux = all_coordinates[u]["x"]
     uy = all_coordinates[u]["y"]
@@ -19,7 +21,8 @@ def weight(u, v, d):
     w = math.sqrt(pow(vx - ux, 2) + pow(vy - uy, 2) + pow(vz - uz, 2))
     return w
 
-'''
+
+"""
 In this context, a stop is either a station or a switch.
 Given properly formatted stop dictionaries, return dictionaries mapping...
 
@@ -31,16 +34,19 @@ stops -> connections
 
 ...returning both dictionaries.
 
-'''
+"""
+
+
 def process(data):
     coordinates = {}
     connections = {}
 
     for stop, stop_data in data.items():
-        coordinates[stop] = stop_data['coordinates']
-        connections[stop] = stop_data['connections']
+        coordinates[stop] = stop_data["coordinates"]
+        connections[stop] = stop_data["connections"]
 
-    return(coordinates, connections)
+    return (coordinates, connections)
+
 
 # Load YAML from files
 station_yaml = yaml.full_load(open("stations.yml", "r"))
@@ -60,18 +66,14 @@ g = nx.from_dict_of_lists(station_connections)
 g.update(nx.from_dict_of_dicts(switch_connections))
 
 # Create a data structure that maps keys NESW to 1-4, respectively
-direction_handler = dict((y,x) for x,y in enumerate(["N", "E", "S", "W"], start=1))
+direction_handler = dict((y, x) for x, y in enumerate(["N", "E", "S", "W"], start=1))
 
 # Calculate the switch configuration required for shortest paths to each station
 out = {}
 for station, coordinates in station_coordinates.items():
     out[station] = {
         "switches": {},
-        "pos": {
-            "x": coordinates["x"],
-            "y": coordinates["y"],
-            "z": coordinates["z"],
-        },
+        "pos": {"x": coordinates["x"], "y": coordinates["y"], "z": coordinates["z"]},
     }
 
     for switch in switch_coordinates.keys():
@@ -80,5 +82,11 @@ for station, coordinates in station_coordinates.items():
         out[station]["switches"][switch] = direction_handler[direction]
 
 # Print results
-print("---")
-print(yaml.dump(out))
+# print("---")
+# print(yaml.dump(out))
+
+with open("sbubby.yml", "w") as out_file:
+    out_file.write("---\n" + yaml.dump(out))
+
+with open("switch_coords.yml", "w") as coordinate_file:
+    coordinate_file.write("---\n" + yaml.dump(switch_coordinates))
